1ec031c9d96caf424e09b1027b9ee02c4570e6e2
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-11-13 19:01:02 -0300

    Correção

--- metadata.js
+++ metadata.js
@@ -1,5 +1,4 @@
 export default {
-	description: 'Auxilia a conferência de RPVs e precatórios.',
 	namespace: 'http://nadameu.com.br/baixa',
 	include: [
 		/^https:\/\/eproc\.(jf(pr|rs|sc)|trf4)\.jus\.br\/eproc(V2|2trf4)\/controlador\.php\?acao=baixa_arquivamento_processo_etapa_(1|3)&/,
