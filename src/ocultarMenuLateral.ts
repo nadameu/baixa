451d238bbb8d5e6ab78a2813b34c20016daa74a7
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-11-22 12:56:06 -0300

    Correção

--- src/ocultarMenuLateral.ts
+++ src/ocultarMenuLateral.ts
@@ -1,11 +1,11 @@
-import { queue } from './queue';
 export function ocultarMenuLateral(wrapper: HTMLElement, sidebar: HTMLElement) {
 	wrapper.style.transition = 'none';
-	sidebar.style.transition = 'none';
+	wrapper.classList.remove('abre-automaticamente');
 	wrapper.classList.add('toggled');
-	queue(() => {
-		wrapper.getBoundingClientRect(); // Forçar paint do navegador
-		wrapper.style.transition = '';
-		sidebar.style.transition = '';
-	});
+	sidebar.style.transition = 'none';
+
+	wrapper.getBoundingClientRect(); // Forçar paint do navegador
+
+	wrapper.style.transition = '';
+	sidebar.style.transition = '';
 }
