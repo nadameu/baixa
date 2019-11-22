cd875a9c62182c173dd65757ef86960b5961dc87
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-11-22 19:03:33 -0300

    Não utilizado

--- src/queue.ts
+++
@@ -1,9 +0,0 @@
-export function queue(microtask: () => void, win = window): void {
-	Promise.resolve()
-		.then(microtask)
-		.catch(err =>
-			win.setTimeout(() => {
-				throw err;
-			}, 0)
-		);
-}
