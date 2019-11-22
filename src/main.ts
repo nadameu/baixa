063a02cfefbc256b4178e41c852126c4a8400fe6
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-11-22 12:56:59 -0300

    Refatoração

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,7 @@ export function main(
 	doc = win.document,
 	storage = win.localStorage
 ) {
-	const acao = new URL(doc.location.href).searchParams.get('acao');
-	const match = (acao === null ? '' : acao).match(
-		/^baixa_arquivamento_processo_etapa_(1|3)$/
-	);
-	const etapa = (match === null ? ['', 'desconhecida'] : match)[1] as string;
+	const { acao, etapa } = extrairDadosUrl(doc.location.href);
 	if (etapa === '1') {
 		etapa1({ win, doc });
 	} else if (etapa === '3') {
@@ -18,4 +14,13 @@ export function main(
 	} else {
 		throw new Error(`Ação desconhecida: '${acao}'.`);
 	}
+}
+
+function extrairDadosUrl(url: string) {
+	const acao = new URL(url).searchParams.get('acao');
+	const match = (acao === null ? '' : acao).match(
+		/^baixa_arquivamento_processo_etapa_(1|3)$/
+	);
+	const etapa = (match === null ? ['', 'desconhecida'] : match)[1];
+	return { acao, etapa };
 }
